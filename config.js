bc703dce068fae26035f795ec7f4794c88802b03
Tx-Nascimento <tiagonascimento1123@gmail.com>
2026-07-10 18:20:32 -0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://mzawmvqrlrptidcwxkoi.supabase.co";
 
-const SUPABASE_KEY = "SUA_CHAVE_ANON_ATUAL";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16YXdtdnFybHJwdGlkY3d4a29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NzU4ODQsImV4cCI6MjA4OTE1MTg4NH0.mdl00txGN4GpXdLsgeANS1m05iTeQaO76jo7yggp004";
 
 const { createClient } = supabase;
 
